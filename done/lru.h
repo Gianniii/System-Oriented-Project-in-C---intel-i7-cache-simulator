547b285cfa28ea31a21a23f3ed5c5c24e19812b8
metaTinker <michael.roust@epfl.ch>
2019-06-03 02:26:29 +0200

    I FOUND THE ******

--- done/lru.h
+++ done/lru.h
@@ -4,9 +4,9 @@
 #include "cache_mng.h"
 
 #define LRU_age_increase(TYPE, WAYS, WAY_INDEX, LINE_INDEX) \
-    foreach_way(i, WAYS) { \
-        TYPE* cache_e = cache_entry(TYPE, WAYS, LINE_INDEX, i); \
-        if (i == WAY_INDEX) { \
+    foreach_way(m_way_iterator, WAYS) { \
+        TYPE* cache_e = cache_entry(TYPE, WAYS, LINE_INDEX, m_way_iterator); \
+        if (m_way_iterator == WAY_INDEX) { \
             cache_e->age = 0; \
         } else { \
             if(cache_e->age < WAYS - 1) cache_e->age =  cache_e->age + 1; \
@@ -15,9 +15,9 @@
 
 #define LRU_age_update(TYPE, WAYS, WAY_INDEX, LINE_INDEX) \
     uint8_t temp = cache_age(TYPE, WAYS, LINE_INDEX, WAY_INDEX); \
-    foreach_way(i, WAYS) { \
-        TYPE* cache_e = cache_entry(TYPE, WAYS, LINE_INDEX, i); \
-        if (i == WAY_INDEX) { \
+    foreach_way(m_way_iterator, WAYS) { \
+        TYPE* cache_e = cache_entry(TYPE, WAYS, LINE_INDEX, m_way_iterator); \
+        if (m_way_iterator == WAY_INDEX) { \
             cache_e->age = 0; \
         } else if (cache_e->age < temp) { \
             cache_e->age++; \
